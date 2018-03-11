74daff8a02d86eb474c647ebcf2a7b727ccb15be
yassimortensen <yassimortensen@gmail.com>
2018-03-11 19:05:18 -0400

    moved profile image to its own component to fix async load

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import profileImage from './images/profile-image.JPG'
 
-const ProfileImage = () => (
-  <img alt="" src={profileImage} className="img-circle" style={{display:'inline-block'}}/>
-)
+class ProfileImage extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      image: null
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      image: profileImage
+    })
+  }
+
+  render(){
+    return (
+      <img alt="" src={this.state.image} className="img-circle" style={{display:'inline-block'}}/>
+    )
+  }
+}
+
 
 export default ProfileImage
