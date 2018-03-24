7a19a55222c5f927b9b14db75749beed24e8d061
yassimortensen <yassimortensen@gmail.com>
2018-03-23 20:10:23 -0400

    removed state from profileImage component

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -3,34 +3,10 @@ import profileImage from './images/profile-image.JPG'
 
 class ProfileImage extends Component {
 
-  constructor(){
-    super();
-
-    this.state = {
-      image: null,
-      className: null
-    }
-  }
-
-  componentDidMount(){
-    this.setState({
-      image: profileImage,
-      className: "img-circle"
-    })
-  }
-
   render(){
 
-    let className;
-
-    if (this.state.image){
-      className = "img-circle"
-    } else {
-      className = null
-    }
-
     return (
-      <img alt="" src={this.state.image} className={className} style={{display:'inline-block'}}/>
+      <img alt="" src={profileImage} className="img-circle" style={{display:'inline-block'}}/>
     )
   }
 }
